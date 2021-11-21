ad6535c6f48dcf8a9b4bff1a6a78b1fa30976770
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2021-11-21 10:45:21 -0300

    TPF Codo a Codo

---
+++ template/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Milos web</title>
+    <link rel="stylesheet" href="./css/bootstrap.css"
+
+</head>
+<body>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+       <ul class="nav navbar-nav">
+           <li class="nav-item active">
+               <a class="nav-link" href="#">Milos Sublimados</a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="Index.php">Inicio</a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="Design.php">Diseños</a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="Redes.php">Redes sociales</a>
+           </li>
+           <li class="nav-item">
+               <a class="nav-link" href="./administrador/Index.php">Login CRUD</a>
+           </li>
+       </ul>
+   </nav>
+   <div class="container">
+       <br>
+       <div class="row">
